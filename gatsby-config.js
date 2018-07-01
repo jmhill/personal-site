9679d19446376d7fb78224178de17971a0430592
Justin Hill <hillj@kcic.com>
2018-06-30 21:34:22 -0300

    chore: add gatsby/netlify config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Justin M. Hill',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify-cms',
+  ],
 }
