01cab7686f03a004c3b53e39790cca61e6343194
Justin Hill <hillj@kcic.com>
2018-07-01 16:16:18 -0300

    build: add plugin config for source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-netlify-cms',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: `${__dirname}/blog`,
+      },
+    },
   ],
 }
