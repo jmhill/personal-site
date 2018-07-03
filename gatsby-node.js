4ed3e13be3fe4a68b0c3b28dbab5f2d9338f2014
Justin Hill <hillj@kcic.com>
2018-07-03 00:25:08 -0300

    feat: add markdown transformer and starter blog template

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,60 @@
  */
 
  // You can delete this file if you're not using it
+
+const path = require("path");
+const { createFilePath } = require('gatsby-source-filesystem');
+
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+};
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
+
+  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.jsx`);
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              path
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: `/blog/${node.frontmatter.path}/`,
+        component: blogPostTemplate,
+        context: {
+          slug: node.fields.slug,
+        }, // additional data can be passed via context
+      });
+    });
+  });
+};
+
---
+++ src/templates/blogTemplate.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default (props) => {
+  console.log(props);
+  const { markdownRemark } = props.data; // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <div className="blog-post-container">
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export const pageQuery = graphql`
+  query BlogPostByPath($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`;
+
