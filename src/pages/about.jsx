1d32a2930154be98fe3d73441af2d0f521178f3d
Justin Hill <hillj@kcic.com>
2018-07-07 16:30:29 -0300

    feat: add about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import { ContentBlock } from '../components/ContentBlock.jsx'
 
-const AboutPage = () => <ContentBlock title="About Me" />
+const AboutPage = ({data}) => {
+  const html = data.allMarkdownRemark.edges[0].node.html;
+
+  return (
+    <ContentBlock title="About Me">
+      <span dangerouslySetInnerHTML={{ __html: html }} />
+    </ContentBlock>
+  );
+};
 
 export default AboutPage
+
+export const AboutPageQuery = graphql`
+  query AboutPage {
+    allMarkdownRemark(
+      filter: {
+        fields: {
+          slug: {
+            eq: "/about/"
+          }
+        }
+      }
+    ) {
+      edges {
+        node {
+          html
+        }
+      }
+    }
+  }
+`
