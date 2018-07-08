403455dbf59f0b2816f541fedb3a8ce648c18c5e
Justin Hill <hillj@kcic.com>
2018-07-08 12:26:14 -0300

    chore: add placeholder page for projects section

---
+++ src/pages/projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ContentBlock } from '../components/ContentBlock';
+
+const ProjectsPage = () => (
+  <ContentBlock title="Projects">
+    This page under construction... check back soon.
+  </ContentBlock>
+);
+
+export default ProjectsPage;
