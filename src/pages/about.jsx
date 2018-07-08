629e1eea5b05fada18340050e76ff2be1632bddc
Justin Hill <hillj@kcic.com>
2018-07-08 18:08:56 -0300

    chore: add proptypes validation

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ContentBlock } from '../components/ContentBlock';
 
 const AboutPage = ({ data }) => {
@@ -10,6 +11,12 @@ const AboutPage = ({ data }) => {
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </ContentBlock>
   );
+};
+
+AboutPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.any,
+  }).isRequired,
 };
 
 export default AboutPage;
