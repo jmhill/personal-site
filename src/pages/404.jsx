f6a6fe33b79342536606fb196999ec57395652ff
Justin Hill <hillj@kcic.com>
2019-07-04 08:25:46 -0300

    chore: update 404 page text

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You have tried to navigate to a page that does not exist.</p>
+    <p>
+      <a href="/">Click here to go back to the home page</a>
+    </p>
   </div>
 );
 
